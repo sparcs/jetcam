9ca9617263400acb1336f918af94d5291428d0ea
Sumukh <sumukh.ghodke@gmail.com>
2021-02-11 19:12:51 +0530

    add package deps and update setup to use jetcam without jetcard env

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
+import os
 from setuptools import setup, find_packages
+
+install_requires = []
+req_file = os.path.sep.join([os.path.realpath(__file__), 'requirements.txt'])
+if os.path.isfile(req_file):
+	with open(req_file) as f:
+		install_requires = f.read().splitlines()
 
 setup(
     name='jetcam',
     version='0.0.0',
     description='An easy to use camera interface for NVIDIA Jetson',
     packages=find_packages(),
-    install_requires=[
-    ],
+    install_requires=install_requires,
 )
